c923bca258d1bbf84c280035351be95b3a1b7f7f
jhlym <jhlym92@gmail.com>
2020-06-29 23:28:05 +0900

    init

--- src/constant/Routes.ts
+++
@@ -1,8 +0,0 @@
-export const routes = {
-  companys: {
-    name: '보험사 정보 조회',
-    pathname: '/insurance/invoices/env',
-  },
-};
-
-export default routes;
--- src/lib/Utils.ts
+++
@@ -1,2 +0,0 @@
-export const isSuccessApiCall = (response) =>
-  response.status >= 200 && response.status < 300 && Object.keys(response.data).length > 0;
