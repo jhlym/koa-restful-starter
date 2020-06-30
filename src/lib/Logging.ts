caea22bbdc789d7b789a4d6cae5fa8f515e8ecfe
jhlym <jhlym92@gmail.com>
2020-06-30 18:16:22 +0900

    refactor: 샘플 파일 수정 및 도커 설정 파일 추가

--- src/lib/Logging.ts
+++ src/lib/Logging.ts
@@ -1,8 +1,5 @@
 import * as winston from 'winston';
 
 export const logger = winston.createLogger({
-  transports: [
-    new winston.transports.Console(),
-    // new winston.transports.File({ filename: 'combined.log' }),
-  ],
+  transports: [new winston.transports.Console()],
 });
